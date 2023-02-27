ff67c0557a4a253f67e6b03aba0f4114ab29fb87
Ahmed Ehab Saleh <a.saleh.eja@ncgr.gov.sa>
2023-02-27 14:56:44 +0200

    Added environment variables & config file

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ const userRouter = require('./routes/userRoutes');
 const toursRouter = require('./routes/tourRoutes');
 
 const app = express();
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware! 👋🏻");
+  console.log('Hello from the middleware! 👋🏻');
   next();
 });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
-})
+});
 
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', userRouter);
