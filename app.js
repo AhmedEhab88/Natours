cc9ad198867491ec7510e9f53361dd2ce7f7232f
AhmedEhab88 <ahmedesrm@gmail.com>
2023-03-01 20:58:53 +0200

    Configured esLint.

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware! 👋🏻");
+  console.log('Hello from the middleware! 👋🏻');
   next();
 });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
-})
+});
 
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', userRouter);
