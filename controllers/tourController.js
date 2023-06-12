899a4e1d0a8734544ed5617dd61ef9d1de9ecca1
AhmedEhab88 <ahmedesrm@gmail.com>
2023-06-12 23:29:26 +0300

    Added comment for clarification

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,23 +14,23 @@
 const Tour = require('../models/tourModel');
 
 exports.getTours = async (req, res) => {
-    try {
-        const tours = await Tour.find();
+  try {
+    const tours = await Tour.find();
 
-      res.status(200).json({
-         status: 'success',
-         requestedAt: req.requestTime,
-         count: tours.length,
-         data: {
-            tours: tours,
-         },
-      });
-   } catch (err) {
-      res.status(404).json({
-         status: 'fail',
-         message: err,
-      });
-   }
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      count: tours.length,
+      data: {
+        tours: tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.getTour = async (req, res) => {
@@ -72,36 +72,36 @@ exports.createTour = async (req, res) => {
 };
 
 exports.updateTour = async (req, res) => {
-   try {
-      const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-         new: true,
-         runValidators: true,
-      });
-      res.status(200).json({
-         status: 'success',
-         data: {
-            tour,
-         },
-      });
-   } catch (err) {
-      res.status(404).json({
-         status: 'fail',
-         message: err,
-      });
-   }
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true, //This is to run all the validators while updating to check if incoming req is correct.
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.deleteTour = async (req, res) => {
-   try {
-      await Tour.findByIdAndDelete(req.params.id);
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
 
-      res.status(204).json({
-         status: 'success',
-      });
-   } catch (err) {
-      res.status(404).json({
-         status: 'fail',
-         message: err,
-      });
-   }
+    res.status(204).json({
+      status: 'success',
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
