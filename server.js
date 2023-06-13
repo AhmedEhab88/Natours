d16a584e906d3ba80df669e5becc26e5b5b7d2f8
Ahmed Ehab Saleh <a.saleh.eja@ncgr.gov.sa>
2023-06-13 16:56:57 +0200

    Added simple API filtering functionality

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
-   '<PASSWORD>',
-   process.env.DATABASE_PASSWORD
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
 );
 
 mongoose
-   .connect(DB, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-   })
-   .then(() => console.log('DB connection successful ✔'));
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful ✔'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-   console.log(`App is running on port ${port}...`);
+  console.log(`App is running on port ${port}...`);
 });
