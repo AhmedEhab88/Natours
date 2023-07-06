2ffaeb90875a0a08e3b582c16de1b16ce9e6e50e
Ahmed Ehab Saleh <a.saleh.eja@ncgr.gov.sa>
2023-07-06 16:23:03 +0200

    Removed unnecessary commented out code.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,55 +10,6 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getTours = async (req, res) => {
     try {
-        // BUILD THE QUERY
-
-        //1A) Filtering
-
-        //Deep copy of the req.query object
-        // const queryObj = { ...req.query };
-        // const excludedFields = ['page', 'limit', 'sort', 'fields'];
-        // excludedFields.forEach((el) => delete queryObj[el]);
-
-        // //1B) Advanced Filtering
-        // let queryStr = JSON.stringify(queryObj);
-        // queryStr = queryStr.replace(
-        //     /\b(gte|gt|lt|lte)\b/g,
-        //     (match) => `$${match}`
-        // );
-
-        // let query = Tour.find(JSON.parse(queryStr));
-
-        // //2) Sorting
-        // if (req.query.sort) {
-        //     const sortBy = req.query.sort.split(',').join(' ');
-        //     query = query.sort(sortBy);
-        // } else {
-        //     //If user did not specify sort parameter,
-        //     //Sort by CreatedAt by default
-        //     query = query.sort('_id');
-        // }
-
-        // //3)Field Limiting (Projection)
-        // if (req.query.fields) {
-        //     const fields = req.query.fields.split(',').join(' ');
-        //     query = query.select(fields);
-        // } else {
-        //     query = query.select('-__v');
-        // }
-
-        // //4) Pagination
-        // const page = req.query.page * 1 || 1;
-        // const limit = req.query.limit * 1 || 100;
-        // const skip = (page - 1) * limit;
-
-        // query = query.skip(skip).limit(limit);
-
-        // if (req.query.page) {
-        //     const totalTours = await Tour.countDocuments();
-        //     if (skip >= totalTours)
-        //         throw new Error('This page does not exist!');
-        // }
-
         //EXECUTE THE QUERY
         const features = new APIFeatures(Tour.find(), req.query)
             .filter()
